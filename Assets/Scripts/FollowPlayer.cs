a374727e740692e0092ae35ff4af12bf222bb1ec
TPteam7 <tplayz07@gmail.com>
2023-10-10 13:24:18 -0700

    Fix stuff and penguin

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,18 +5,29 @@ using UnityEngine.AI;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public Transform target;
-    NavMeshAgent nav;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        nav = GetComponent<NavMeshAgent>();
-    }
+    public GameObject Player;
+    public float TargetDistance;
+    public float AllowedDistance = 5;
+    public GameObject Penguin;
+    public float FollowSpeed;
+    public RaycastHit Shot;
 
     // Update is called once per frame
     void Update()
     {
-        nav.SetDestination(target.position);
+        transform.LookAt(Player.transform);
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Shot))
+        {
+            TargetDistance = Shot.distance;
+            if (TargetDistance >= AllowedDistance) 
+            {
+                FollowSpeed = .02f;
+                transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, FollowSpeed);
+            }
+            else
+            {
+                FollowSpeed = 0;
+            }
+        }
     }
 }
