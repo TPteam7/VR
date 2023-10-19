bceb2038a144b1b2d9477a8feee57293bd35e21c
MichealC03 <micheal.callahan@icloud.com>
2023-10-19 11:09:31 -0700

    new commit to branch

---
+++ Assets/Scripts/Push.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Push : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forceStrength = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(Vector3.up * forceStrength);
+    }
+}
