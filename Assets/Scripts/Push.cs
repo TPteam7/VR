e92c0a9b1ea1c8d20ca6025f086c3b4598055d40
MichealC03 <micheal.callahan@icloud.com>
2023-11-02 22:53:38 -0700

    target area added

--- Assets/Scripts/Push.cs
+++ Assets/Scripts/Push.cs
@@ -5,17 +5,44 @@ using UnityEngine;
 public class Push : MonoBehaviour
 {
     public Rigidbody rb;
-    public float forceStrength = 20;
+    public float forceStrength;
+    public Collider Target; // Assign your trigger zone here
+    private bool isStopped;
+    private float stopTime;
+    public float stopDuration = 5.0f;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.left * forceStrength, ForceMode.Impulse);
+        Debug.Log("Object has stopped for 5 seconds.");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        rb.AddForce(Vector3.up * forceStrength);
+        // Check if the object is within the trigger zone
+        if (Target.bounds.Contains(transform.position))
+        {
+            // If it's in the zone, start or continue timing
+            if (!isStopped)
+            {
+                isStopped = true;
+                stopTime = Time.time;
+            }
+            else
+            {
+                // If it has been in the zone for the specified duration, trigger your event
+                if (Time.time - stopTime >= stopDuration)
+                {
+                    // Add your event code here
+                    Debug.Log("Object has stopped for 5 seconds.");
+                }
+            }
+        }
+        else
+        {
+            // If it's not in the zone, reset the timing
+            isStopped = false;
+        }
     }
 }
