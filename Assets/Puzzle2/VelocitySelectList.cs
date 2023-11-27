b442c35d0c945a59219b0053aa562eeeb53c2156
MichealC03 <micheal.callahan@icloud.com>
2023-11-27 14:33:35 -0700

    added a select list

---
+++ Assets/Puzzle2/VelocitySelectList.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocitySelectList : MonoBehaviour
+{
+    public Rigidbody rb; // Reference to the Rigidbody of the ball
+    public float velocity1 = 9f; // Adjust these values based on your requirements
+    public float velocity2 = 10f;
+    public float velocity3 = 15f;
+
+    public void SetVelocityFromIndex(int index)
+    {
+        switch (index)
+        {
+            case 1:
+                rb = GetComponent<Rigidbody>();
+                rb.AddForce(Vector3.left * velocity1, ForceMode.Impulse);
+                break;
+            case 2:
+                rb = GetComponent<Rigidbody>();
+                rb.AddForce(Vector3.left * velocity2, ForceMode.Impulse);
+                break;
+            case 3:
+                rb = GetComponent<Rigidbody>();
+                rb.AddForce(Vector3.left * velocity3, ForceMode.Impulse);
+                break;
+            default:
+                Debug.LogWarning("Invalid index selected");
+                break;
+        }
+    }
+}
